c964fa4b1ec379feeca246c3f9bc533f3e43dc84
R Narayan <nr.rnarayan@gmail.com>
2020-06-27 15:24:50 +0530

    motor

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-        finally:
-            #Close down curses properly, inc turn echo back on!
-            curses.nocbreak(); screen.keypad(0); curses.echo()
-            curses.endwin()
-            # GPIO.cleanup()
